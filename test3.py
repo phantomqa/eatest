580a736055ead6db739bac2a64eae3cff5f8fa19
admin <root@localhost>
2016-06-21 22:48:49 +0000

    asdf

---
+++ test3.py
@@ -0,0 +1,21 @@
+import phantom.rules as phantom
+import json
+
+def on_start(container):
+    (container)
+
+    return
+
+
+def on_finish(container, summary):
+
+    # This function is called after all actions are completed.
+    # Summary and/or action results can be collected here.
+
+    # summary_json = phantom.get_summary()
+    # summary_results = summary_json['result']
+    # for result in summary_results:
+            # action_run_id = result['id']
+            # action_results = phantom.get_action_results(action_run_id=action_run_id)
+
+    return
